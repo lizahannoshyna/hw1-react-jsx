f47c69c992f3c1de2e2db53a566c6530214caaef
Liza Hannoshyna <fox8pro8@gmail.com>
2025-11-22 09:41:48 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/hw1-react-jsx/',
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()]
 })
+
